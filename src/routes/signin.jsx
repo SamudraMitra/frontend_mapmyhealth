7e7a0580bcf097288cc2af53f30db297698e2fc4
Samudra <samudramitra2017@gmail.com>
2023-06-17 21:20:35 +0530

    prevented deployment massacre

--- src/routes/signin.jsx
+++ src/routes/signin.jsx
@@ -1,20 +1,20 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
+import * as React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // import { Link } from 'react-router-dom';
@@ -22,13 +22,18 @@ const BACKEND_URL = `${process.env.REACT_APP_BACKEND_URL}/login`;
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -40,33 +45,32 @@ export default function SignIn() {
 
   const [data, setData] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setData({ ...data, [name]: value });
-  }
+  };
   const handleSubmit = async (event) => {
     try {
       event.preventDefault();
       console.log(data);
       const res = await axios.post(BACKEND_URL, data);
       const userData = res.data;
-      localStorage['data'] = JSON.stringify(userData);
+      localStorage["data"] = JSON.stringify(userData);
       toast.success(res.data.message);
       setTimeout(() => {
-        navigate('/home');
-      }, 2000)
-    }
-    catch (error) {
+        navigate("/home");
+      }, 2000);
+    } catch (error) {
       console.log(error);
       // toast.error(error.response.data.message);
     }
   };
 
   return (
-    <div className='mt-32'>
+    <div className="mt-32">
       <ToastContainer />
       <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs">
@@ -74,12 +78,12 @@ export default function SignIn() {
           <Box
             sx={{
               marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
@@ -96,9 +100,8 @@ export default function SignIn() {
                 autoComplete="email"
                 autoFocus
                 onChange={(e) => {
-                  handleChange(e)
+                  handleChange(e);
                 }}
-
               />
               <TextField
                 margin="normal"
@@ -110,7 +113,7 @@ export default function SignIn() {
                 id="password"
                 autoComplete="current-password"
                 onChange={(e) => {
-                  handleChange(e)
+                  handleChange(e);
                 }}
               />
               <FormControlLabel
@@ -122,16 +125,15 @@ export default function SignIn() {
                 fullWidth
                 variant="contained"
                 onClick={(e) => {
-                  handleSubmit(e)
+                  handleSubmit(e);
                 }}
                 sx={{ mt: 3, mb: 2 }}
               >
                 Sign In
               </Button>
               <Grid container>
-
                 <Grid item>
-                  <Link href='/signup' variant="body2">
+                  <Link href="../signup" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
